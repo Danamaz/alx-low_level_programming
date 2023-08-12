f97b631b6ccf76a79d8413a79b137b464bed3e43
Danamaz <danayebidazeph@gmail.com>
2023-08-12 15:26:01 -0700

    Updating 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
- /**
+
+/**
  * main - Entry point
- * Return: Always 0
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Programmming is like building a multilingual puzzle");
+	puts("\"Programmming is like building a multilingual puzzle");
 	return (0);
 }
