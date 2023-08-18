24bc3c481c4e63eb8071e9b9dfc72815555b7a76
Danamaz <105615670+Danamaz@users.noreply.github.com>
2023-08-18 02:05:36 +0000

    Update 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *print_square - it prints squares
+ *@size: value to work with
+ *Return: Always 0.
+ */
+
+void print_square(int size)
+{  
+	int i; 
+
+	for (i = 0; i < abs(size); i++)
+	{
+	for (i = 0; i < abs(size); i++)
+		_putchar('\#');
+	}
+    		_putchar('\n');
+	}	
+	_putchar('\n');
+}
