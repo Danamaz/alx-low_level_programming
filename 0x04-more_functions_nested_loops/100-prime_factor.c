e04e632fbb7556c32f3c36ce4002ec480438a6c4
Danamaz <danayebidazeph@gmail.com>
2023-08-17 21:07:53 -0700

    Making Update

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long long int n = 612852475143;
+	long int div = 2, maxFact;
+
+	while (n != 0)
+	{
+	if (n % div != 0)
+	div = div + 1;
+	else
+	{
+	maxFact = n;
+	n = n / div;
+	if (n == 1)
+	{
+	printf ("%lld ", maxFact);
+	break;
+	}
+	}
+	}
+	return (0);
+}
