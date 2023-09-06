ce02da27ba9967065ffc3e800c6dcbf96663d591
Danamaz <danayebidazeph@gmail.com>
2023-09-05 19:17:55 -0700

    Memory Allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,16 +14,17 @@ char *create_array(unsigned int size, char c)
 {
 	if (size == 0)
 	{
-	return NULL;
+	return (NULL);
 	}
 	char *array = (char *)malloc(size * sizeof(char));
+
 	if (array == NULL)
 	{
-	return NULL;
+	return (NULL);
 	}
 	for (unsigned int i = 0; i < size; i++)
 	{
 	array[i] = c;
 	}
-	return array;
+	return (array);
 }
