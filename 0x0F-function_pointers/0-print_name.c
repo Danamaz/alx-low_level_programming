7f717419f8152b6848868059243038f184872f00
Danamaz <105615670+Danamaz@users.noreply.github.com>
2023-09-13 19:08:07 -0700

    adding executable command

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,15 @@
-#include "function_pointers_h"
-#include <stdlib.h>
-
+#include "function_pointers.h"
+#include <stdio.h>
 /**
- * print_name - a function that prints a name
- * @name: A pointer to a string containing the name to print
- * @f: a pointer to a funtion that takes a pointer to character array
- */
-
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+ **/
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
+
 	f(name);
 }
